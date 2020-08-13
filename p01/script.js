9d2c8695a0067cdfcb7fbf7dd04d12843625694b
muhammadazhar22 <41356674+muhammadazhar22@users.noreply.github.com>
2020-08-13 06:09:11 +0500

    Updated script.js
    Added email validation with simple JS

--- p01/script.js
+++ p01/script.js
@@ -1,46 +1,57 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const password2 = document.getElementById('password2')
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
 // All functions
 
 //Function to show error
 function showError(input, massage) {
-    const formControl = input.parentElement
-    formControl.className = 'form-control error'
-    const small = formControl.querySelector('small')
-    small.innerText = massage
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = massage;
 }
 
 // Function to show success
 function showSuccess(input) {
-    const formControl = input.parentElement
-    formControl.className = 'form-control success'
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+// Function to check if email is valid
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 // This is an event listener for the form on submit
-form.addEventListener('submit', function(e) {
-    e.preventDefault()
-    if (username.value === '') {
-        showError(username, 'Username is required')
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    if (username.value === "") {
+        showError(username, "Username is required");
     } else {
-        showSuccess(username)
+        showSuccess(username);
     }
-    if (email.value === '') {
-        showError(email, 'email is required')
+
+    if (email.value === "") {
+        showError(email, "Email is required");
+    } else if (!isValidEmail(email.value)) {
+        showError(email, "Email is invalid");
     } else {
-        showSuccess(email)
+        showSuccess(email);
     }
-    if (password.value === '') {
-        showError(password, 'password is required')
+
+    if (password.value === "") {
+        showError(password, "password is required");
     } else {
-        showSuccess(password)
+        showSuccess(password);
     }
-    if (password2.value === '') {
-        showError(password2, 'confirming password is required')
+
+    if (password2.value === "") {
+        showError(password2, "confirming password is required");
     } else {
-        showSuccess(password2)
+        showSuccess(password2);
     }
-})
+});
