6a97279e06254721b06f82fe8198f2a0df331409
muhammadazhar22 <41356674+muhammadazhar22@users.noreply.github.com>
2020-08-13 07:58:16 +0500

    Update script.js
    Added functionality to check email
    validation, check length validation, and
    also password match validation

--- p01/script.js
+++ p01/script.js
@@ -21,9 +21,14 @@ function showSuccess(input) {
 }
 
 // Function to check if email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, `Please provide a valid email`);
+    }
+
 }
 // Function to check if required feilds have data
 function checkRequired(inputArray) {
@@ -37,15 +42,41 @@ function checkRequired(inputArray) {
 
 }
 
+//Function to check length of input field
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldId(input)} needs to be less than ${min} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Function to check if password and confirm password match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, "Password don't match");
+    }
+}
+
 // Function to get the id of the input field with proper case
 function getFieldId(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
+
+
 
 // This is an event listener for the form on submit
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 10);
+    checkLength(password, 6, 30);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+
+
 
 });
